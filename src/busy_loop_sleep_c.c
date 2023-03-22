0af388504d53f7f87722e56210a456fc737f75df
Fred <fred.chen@live.com>
2023-03-22 01:46:34 +0000

    add more src code.

--- src/busy_loop_sleep_c.c
+++ src/busy_loop_sleep_c.c
@@ -13,3 +13,4 @@ int main(int argc, char *argv[]) {
   slp();
   return 0;
 }
+
---
+++ src/stack.c
@@ -0,0 +1,7 @@
+int add(int x, int y) { return x + y; }
+
+int main(int argc, char *argv[]) {
+  int a = 1, b = 2;
+  int c = add(a, b);
+  return 0;
+}
